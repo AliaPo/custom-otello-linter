bd8c1e318d48b71cbb0f31876a81829574540ed6
a.krashchenko <a.krashchenko@2gis.ru>
2025-08-05 16:02:01 +0300

    new fixes and tests

--- custom_otello_linter/visitors/steps_checkers/platform_param_and_method_argument_check.py
+++ custom_otello_linter/visitors/steps_checkers/platform_param_and_method_argument_check.py
@@ -20,27 +20,54 @@ class PlatformParamsChecker(StepsChecker):
                 break
             if isinstance(step, ast.FunctionDef) and step.name == '__init__':
                 init_found = True
+
                 # Проходим по списку декораторов и ищем в них вызовы params с атрибутом Platforms
                 for decorator in step.decorator_list:
-                    if isinstance(decorator, ast.Call) and decorator.func.id == 'params':
+
+                    # Для декораторов вида "@params[allure_labels(AllureID('808960'))](Platforms.MOBILE)"
+                    if isinstance(decorator.func, ast.Subscript):
+                        params_attr = decorator.func.value.id
+                    # Для декораторов вида "@params(Platforms.MOBILE)"
+                    else:
+                        params_attr = decorator.func.id
+
+                    if params_attr == 'params':
                         for arg in decorator.args:
-                            if isinstance(arg, ast.Attribute) and arg.value.id == 'Platforms':
+                            if (
+                                    isinstance(arg, ast.Attribute)
+                                    and isinstance(arg.value, ast.Name)
+                                    and arg.value.id == 'Platforms'
+                            ):
                                 platform_param_present = True
                                 break
 
         if platform_param_present:
             for step in context.steps:
                 # Проверяем, что шаг является функцией и начинается с 'given' или 'when'
-                if ((isinstance(step, ast.AsyncFunctionDef) or isinstance(step, ast.FunctionDef))
-                   and (step.name.startswith('given') or step.name.startswith('when'))):
-
-                    for element in step.body:
-                        # Проверяем, что в теле шага есть вызов функции с параметром platform
-                        if isinstance(element, ast.Assign) and isinstance(element.value, ast.Await):
+                if (
+                        (isinstance(step, ast.AsyncFunctionDef) or isinstance(step, ast.FunctionDef))
+                        and (step.name.startswith('given') or step.name.startswith('when'))
+                ):
+                    # Среди действий в шаге ищем присвоение с await методом, например:
+                    # self.page = await opened_dashboard()
+                    for element in ast.walk(step):
+                        if (
+                                isinstance(element, ast.Assign)
+                                and isinstance(element.value, ast.Await)
+                                and isinstance(element.value.value, ast.Call)
+                        ):
+                            # Проверяем, что в теле шага есть вызов функции с keyword параметром platform
                             for kw_arg in element.value.value.keywords:
                                 if kw_arg.arg == 'platform':
                                     return []
+                            # Если не нашли keyword = platform, проверяем наличие позиционного аргумента
+                            for param in ast.walk(element.value):
+                                # Ищем атрибут без вложенности, только self.platform
+                                if isinstance(param, ast.Attribute) and isinstance(param.value, ast.Name):
+                                    if param.value.id == 'self' and param.attr == 'platform':
+                                        return []
+
             # Если не нашли вызов функции с параметром platform, возвращаем ошибку
-            return [MissingPlatformArgError(lineno=1488, col_offset=0)]
+            return [MissingPlatformArgError(lineno=0, col_offset=0)]
 
         return []
--- tests/test_platform_param.py
+++ tests/test_platform_param.py
@@ -5,19 +5,51 @@ from custom_otello_linter.visitors import ScenarioVisitor
 from custom_otello_linter.visitors.steps_checkers.platform_param_and_method_argument_check import PlatformParamsChecker
 
 
-def test_scenario_with_platform_param_used():
+def test_scenario_with_platform_param_used_as_kw_arg_in_mocked_context():
     ScenarioVisitor.deregister_all()
     ScenarioVisitor.register_steps_checker(PlatformParamsChecker)
     code = """
     class Scenario(vedro.Scenario):
 
-        @params(Platforms.DESKTOP)
+        @params[allure_labels(AllureID('808960'))](Platforms.DESKTOP)
         @params(Platforms.MOBILE)
         def __init__(self, platform):
             pass
 
         def given_opened_page(self):
-            self.page = open_page(booking=self.booking, platform=self.platform)
+            with mocked_page():
+                self.page = await open_page(booking=self.booking, platform=self.platform)
+    """
+    assert_not_error(ScenarioVisitor, code)
+
+
+def test_scenario_with_platform_param_used_as_pos_arg():
+    ScenarioVisitor.deregister_all()
+    ScenarioVisitor.register_steps_checker(PlatformParamsChecker)
+    code = """
+    class Scenario(vedro.Scenario):
+
+        @params[allure_labels(AllureID('808960'))](Platforms.DESKTOP)
+        @params(Platforms.MOBILE)
+        def __init__(self, platform):
+            pass
+
+        def given_opened_page(self):
+            self.page = await open_page(self.booking, self.platform)
+    """
+    assert_not_error(ScenarioVisitor, code)
+
+
+def test_scenario_without_platform_param():
+    ScenarioVisitor.deregister_all()
+    ScenarioVisitor.register_steps_checker(PlatformParamsChecker)
+    code = """
+    class Scenario(vedro.Scenario):
+        def __init__(self, platform):
+            pass
+
+        def given_opened_page(self):
+            self.page = await open_page(self.booking)
     """
     assert_not_error(ScenarioVisitor, code)
 
@@ -34,6 +66,24 @@ def test_scenario_with_platform_param_not_used():
             pass
 
         def given_opened_page(self):
-            self.page = open_page(booking=self.booking)
+            self.page = await open_page(booking=self.booking)
+    """
+    assert_error(ScenarioVisitor, code, MissingPlatformArgError)
+
+
+def test_scenario_with_platform_param_not_used_and_mocked_context():
+    ScenarioVisitor.deregister_all()
+    ScenarioVisitor.register_steps_checker(PlatformParamsChecker)
+    code = """
+    class Scenario(vedro.Scenario):
+
+        @params(Platforms.DESKTOP)
+        @params(Platforms.MOBILE)
+        def __init__(self, platform):
+            pass
+
+        def given_opened_page(self):
+            with mocked_page():
+                self.page = await open_page(booking=self.booking)
     """
     assert_error(ScenarioVisitor, code, MissingPlatformArgError)
